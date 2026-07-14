25a12d183cb89c45330ffc7f8ce9e1e9f3c1d66d
Philipp Huber <Philipp_huber91@gmx.de>
2026-07-14 12:21:55 +0200

    Version 0.0.2 - Progress Update

---
+++ script4.js
@@ -0,0 +1,50 @@
+//Bereiche
+const auftrag = document.getElementById("auftrag");
+const deutsch = document.getElementById("deutsch");
+const thai = document.getElementById("thai");
+const quest = document.getElementById("quest")
+//Buttons
+const startDeutsch = document.getElementById("startDeutsch");
+const startThai = document.getElementById("startThai");
+const questFertig = document.getElementById("questFertig");
+const speichern = document.getElementById("speichern");
+//Value
+const zeit = document.getElementById("zeit");
+const wpm = document.getElementById("wpm");
+const genauigkeit = document.getElementById("genauigkeit");
+//Auftrag - Deutsch
+startDeutsch.addEventListener("click", function () {
+    quest.style.display = "none";
+    auftrag.style.display = "none";
+    deutsch.style.display = "block";
+});
+
+//Auftrag - Thai
+startThai.addEventListener("click", function () {
+    quest.style.display = "none";
+    deutsch.style.display = "none";
+    thai.style.display = "block";
+
+});
+
+//Quest fertig
+questFertig.addEventListener("click", function () {
+
+    thai.style.display = "none";
+    quest.style.display = "block";
+
+});
+
+
+
+speichern.addEventListener("click", function () {
+
+    localStorage.setItem("quest4_zeit", zeit.value);
+    localStorage.setItem("quest4_wpm", wpm.value);
+    localStorage.setItem("quest4_genauigkeit", genauigkeit.value);
+
+});
+
+zeit.value = localStorage.getItem("quest4_zeit");
+wpm.value = localStorage.getItem("quest4_wpm");
+genauigkeit.value = localStorage.getItem("quest4_genauigkeit");
