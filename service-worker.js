a38f838f968fb795996ff2699b41ea9723460640
Philipp Huber <Philipp_huber91@gmx.de>
2026-09-06 08:44:38 +0200

    Align service worker cache version to v175

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@
  * müssen den Cache in Safari nicht manuell löschen.
  */
 
-const CACHE_VERSION = 'v156';
+const CACHE_VERSION = 'v175';
 const CACHE_NAME = `thai-language-grinding-${CACHE_VERSION}`;
 
 // App-Shell: alles, was für Start und Offline-Betrieb gebraucht wird.
